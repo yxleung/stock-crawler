8ba46dea60d7d6d93a5de4637a957265c95bea22
yxleung <liangyuxin.02@gmail.com>
2019-12-03 00:00:58 +0800

    update

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 docker run \
     --name stock-crawler \
-    -v /nas/xiaomi/projects/stock/logs:/opt/project/logs \
-    -v /nas/xiaomi/projects/stock/data:/opt/project/data \
+    -v /home/lyx/projects/stock/logs:/opt/project/logs \
+    -v /home/lyx/projects/stock/data:/opt/project/data \
     -d \
     --restart=always \
     --dns 114.114.114.114 \
